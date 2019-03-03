134e0448a5fe71819902145dd4935a56fbdba674
Jay Chesterman <jay.chesterman@live.ca>
2019-03-02 23:50:59 -0500

    adding gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,9 +42,30 @@ module.exports = {
     },
     'gatsby-plugin-emotion',
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        trackingId: 'UA-135367724-1'
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-135367724-1', // Google Analytics / GA
+          'AW-759754651' // Google Ads / Adwords / AW
+          //'DC-FLOODIGHT_ID' // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared accross all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true
+          // Avoids sending pageview hits from custom paths
+          //exclude: ['/preview/**', '/do-not-track/me/too/']
+        }
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
